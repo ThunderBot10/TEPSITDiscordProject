a4633ba7e8f23bba703ed27547f6a3ffd58cc8fb
ThunderBot10 <ebakassascuola@gmail.com>
2025-05-04 19:59:48 +0200

    upgraded

--- Discord.c
+++ Discord.c
@@ -3,11 +3,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <string.h>
 
 struct Messaggio
 {
-    char *mitt[4];
-    char *mess[4096];
+    char mitt[4];
+    char mess[4096];
 };
 
 void stampaErrore(int tipo);
@@ -20,71 +21,108 @@ int main(int argc, char *argv[])
     /* named pipe per gli utenti */
     int fdut[5];
 
-    int pid; /* pid necessario per la fork */
-    int fd[2]; /* unamed pipe per il processo */
-
-    if (mkfifo(pipe1, 0666) == -1){
-        stampaErrore(1);
-        exit(EXIT_FAILURE);
+    mkdir("/tmpPipe", 0777); // Ensure the directory exists
+    for (int i = 0; i < 5; i++)
+    {
+        if (mkfifo(pipes[i], 0666) == -1)
+        {
+            stampaErrore(1);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    for(int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
-        pid = fork(); /* creazione del processo figlio */
-        /* controllo di possibili errori nella esecuzione della fork */
-        if(pid == -1)
+        int pid = fork(); /* creazione del processo figlio */
+        if (pid == -1)
         {
-            /* chiusura del programma in caso di errore */
             stampaErrore(2);
             exit(EXIT_FAILURE);
         }
 
-        /* processo figlio che andrà ad occuparsi della ricezione ed attesa di un messaggio proveniente dall'esterno */
-        if(pid == 0)
+        if (pid == 0)
         {
-            /* apertura delle named pipe */
             fdut[i] = open(pipes[i], O_RDONLY);
-
-            if(fdut[i] == -1)
+            if (fdut[i] == -1)
             {
                 stampaErrore(3);
                 exit(EXIT_FAILURE);
             }
 
-            char *buffer[4096];
-
-            buffer = read(fdut[i], *buffer, sizeof(buffer));
-            while(!(strcmp(buffer, "/exit")))
+            struct Messaggio messaggio;
+            ssize_t bytes_read;
+            while ((bytes_read = read(fdut[i], messaggio.mess, sizeof(messaggio.mess) - 1)) > 0)
             {
-
+                messaggio.mess[bytes_read] = '\0';
+                if (strcmp(messaggio.mess, "/exit") == 0)
+                {
+                    break;
+                }
+                printf("Messaggio ricevuto da %s: %s\n", messaggio.mitt, messaggio.mess);
             }
+
+            close(fdut[i]);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            fdut[i] = open(pipes[i], O_WRONLY);
+            if (fdut[i] == -1)
+            {
+                stampaErrore(3);
+                exit(EXIT_FAILURE);
+            }
+
+            char message[4096];
+            while (1)
+            {
+                printf("Inserire il messaggio (digita '/exit' per terminare): ");
+                scanf(" %[^\n]", message);
+        
+                if (strcmp(message, "/exit") == 0)
+                {
+                    if (write(fdut[i], message, strlen(message) + 1) == -1)
+                    {
+                        stampaErrore(4);
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                }
+        
+                if (write(fdut[i], message, strlen(message) + 1) == -1)
+                {
+                    stampaErrore(4);
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            close(fdut[i]);
         }
     }
-    
 
+    return 0;
 }
 
-void stampaErrore(int tipo)
-{
-    perror("Errore nell'esecuzione del programma.");
-    switch (tipo)
-    {
+void stampaErrore(int tipo) {
+    perror("Errore nell'esecuzione del programma.\n");
+    switch (tipo) {
         case 1:
-            perror("Errore: non è stata creata correttamente la named pipe.");
+            perror("Errore: non è stata creata correttamente la named pipe.\n");
             break;
-        
+
         case 2:
-            perror("Errore: impossibile creare il processo figlio.");
+            perror("Errore: impossibile creare il processo figlio.\n");
             break;
 
         case 3:
-            perror("Errore: impossibile aprire una o più named pipe.");
+            perror("Errore: impossibile aprire una o più named pipe.\n");
             break;
+
+        case 4:
+            perror("Errore: non è stato possibile scrivere sulla pipe.\n");
 
         default:
             break;
     }
-
-    printf("Chiusura del programma...");
-    return;
+    printf("Chiusura del programma...\n");
 }
